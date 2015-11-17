21a5b8bc5f3301fc58a23884f8ad9da4b910f16b
nbp8557 <nbp8557@rit.edu>
2015-11-17 13:32:12 -0500

    Created Classes for all of the actors in a Line. Also created the jail class

---
+++ Line.java
@@ -0,0 +1,14 @@
+
+import akka.actor.*;
+
+public class Line{
+
+    public ActorRef QueueActor;
+    public ActorRef BodyScanActor;
+    public ActorRef BagScanActor;
+    public ActorRef SecurityActor;
+
+    public Line(){
+
+    }
+}
